45d4c7276be8a392457ed22926c19ea1c1747dbc
kindsecret <forever.xfactor@gmail.com>
2024-09-07 21:23:17 -0400

    feat: add initialize instruction

--- programs/presale-contract/src/lib.rs
+++ programs/presale-contract/src/lib.rs
@@ -2,15 +2,22 @@ use anchor_lang::prelude::*;
 
 declare_id!("82Jdd3gmbvTNCbu2SYGRNkvcESCYgaA6NHBUzbLaH5oh");
 
+mod errors;
+mod instructions;
+
 #[program]
 pub mod presale_contract {
     use super::*;
+    pub use super::instructions::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        token_name: String,
+        token_symbol: String,
+        token_uri: String,
+    ) -> Result<()> {
+        instructions::initialize(ctx, token_name, token_symbol, token_uri)
     }
+    
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
